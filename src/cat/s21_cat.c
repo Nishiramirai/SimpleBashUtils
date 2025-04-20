ad1c4f2f22a317d33f16fea9a2db71e6579705a6
guitarco <guitarco@student.21-school.ru>
2025-04-21 02:08:25 +0300

    Finished basic cat

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -6,26 +6,97 @@ int main(int argc, char *argv[]) {
         return FUNC_ERR_CODE;
     }
 
-    for (int i = 1; i < argc; i++) {
+    options_t opts = get_options(argc, argv);
+    if (opts.invalid_opt) {
+        print_info(argv[0]);
+        return FUNC_ERR_CODE;
+    }
+
+    if (optind >= argc) {
+        printf("File name not specified\n");
+        print_info(argv[0]);
+        return FUNC_ERR_CODE;
+    }
+
+    for (int i = optind; i < argc; i++) {
         s21_cat(argv[i]);
     }
     
+
+    return FUNC_OK_CODE;
 }
 
 void print_info(char *program_name) {
     printf("Usage: %s [OPTION] [FILE]\n", program_name);
 }
 
-int s21_cat(char *filename) {
+void s21_cat(char *filename) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        return FUNC_ERR_CODE;
+        printf("Can't open file %s\n", filename);
+        return;
     }
 
     int c;
     while ((c = fgetc(file)) != EOF) {
         printf("%c", c);
     }
+}
 
-    return FUNC_OK_CODE;
+options_t get_options(int argc, char *argv[]) {
+    static struct option long_options[] = {
+        {"number-nonblank", no_argument, 0, 'b'},
+        {"number", no_argument, 0, 'n'},
+        {"squeeze-blank", no_argument, 0, 's'},
+        {0, 0, 0, 0}
+    };
+
+    const char *opts_list = "benstET";
+
+    options_t opts = {0};
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, opts_list, long_options, NULL)) != -1) {
+        switch (opt)
+        {
+        case 'b':
+            opts.number_nonblank = true;
+            break;
+
+        case 'e':
+            opts.show_ends = true;
+            opts.show_nonprinting = true;
+            break;
+
+        case 'E':
+            opts.show_ends = true;
+            break;
+
+        case 'n':
+            opts.number_lines = true;
+            break;
+
+        case 's':
+            opts.squeeze_blank = true;
+            break;
+
+        case 't':
+            opts.show_tabs = true;
+            opts.show_nonprinting = true;
+            break;
+
+        case 'T':
+            opts.show_tabs = true;
+            break;
+
+        case '?':
+            opts.invalid_opt = true;
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    return opts;
 }
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,9 +1,24 @@
+#include <getopt.h>
 #include <stdio.h>
+#include <stdbool.h>
+
 
 #define FUNC_OK_CODE 0
 #define FUNC_ERR_CODE 1
 
 
-int s21_cat(char *filename);
+typedef struct {
+    bool number_nonblank;
+    bool show_ends;
+    bool number_lines;
+    bool squeeze_blank;
+    bool show_tabs;
+    bool show_nonprinting;
+    bool invalid_opt;
+} options_t;
+
+
+options_t get_options(int argc, char *argv[]);
+void s21_cat(char *filename);
 
 void print_info(char *program_name);
